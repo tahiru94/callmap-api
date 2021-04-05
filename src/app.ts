100bb1c615b67c5274d33cc688ab27f6591b0914
tahiru94 <tahiru1994@engineer.com>
2021-04-05 19:30:40 -0400

    Set up the base Express API
    * Add run / build scripts in package.json
    * Add the base Express App

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application } from 'express';
+import { json, urlencoded } from 'body-parser';
+
+class App {
+    public app: Application;
+
+    constructor() {
+        this.app = express();
+        this.config();
+    }
+
+    private config(): void {
+        this.app.use(json());
+        this.app.use(urlencoded({ extended: false }));
+    }
+}
+
+export default new App().app;
